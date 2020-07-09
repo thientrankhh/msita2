8f278adb6785ed0b24fdbca6d2bb2bf3e0232832
VoDung <vtdung@sdc.udn.vn>
2020-07-09 09:37:48 +0700

    remote relationship teacher and course

--- Modules/Course/Database/Migrations/2020_07_08_031433509110_create_course_courses_table.php
+++ Modules/Course/Database/Migrations/2020_07_08_031433509110_create_course_courses_table.php
@@ -25,9 +25,9 @@ class CreateCourseCoursesTable extends Migration
             $table->boolean('status')->default(0)->comment('0- Không nổi bật, 1- Nổi bật');
             $table->string('slug');
             $table->unsignedBigInteger('category_id');
-            $table->unsignedBigInteger('teacher_id');
+            // $table->unsignedBigInteger('teacher_id');
             $table->foreign('category_id')->references('id')->on('course__categories');
-            $table->foreign('teacher_id')->references('id')->on('course__teachers');
+            // $table->foreign('teacher_id')->references('id')->on('course__teachers');
             $table->timestamps();
         });
     }
